7958d406e897c23d40a35af8d398c4a9438dd414
kamsuri <kamsuri5@gmail.com>
2017-03-25 02:32:43 +0530

    Crawl script added

---
+++ crawl.py
@@ -0,0 +1,25 @@
+import urllib
+from bs4 import BeautifulSoup
+import urlparse
+import requests
+
+
+url = raw_input("Enter a website to extract the URL's from: ")
+url="http://"+url+"/";
+urls = [url]
+visited = [url]
+print url
+while len(urls)>0:
+    try:
+        html=urllib.urlopen(urls[0]).read()
+    except:
+        print "error"
+    urls.pop(0)
+    print len(urls)
+    soup=BeautifulSoup(html, "html.parser")
+    for tag in soup.findAll('a',href=True):
+        tag['href']=urlparse.urljoin(url,tag['href'])
+        if url in tag['href'] and tag['href'] not in visited:
+            urls.append(tag['href'])
+            visited.append(tag['href'])
+print visited
